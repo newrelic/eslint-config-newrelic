1d3d56e3cf13b6aa030bd936d8449e39296109d3
Bob Evans <robert.evans25@gmail.com>
2022-09-13 15:57:07 -0400

    updated esm ruleset for versioned tests

--- index.js
+++ index.js
@@ -108,6 +108,8 @@ module.exports = {
         'node/no-extraneous-require': 'off',
         'node/no-missing-require': 'off',
         'node/no-unpublished-require': 'off',
+        'node/no-unpublished-import': 'off',
+        'node/no-extraneous-import': 'off'
       }
     },
     {
