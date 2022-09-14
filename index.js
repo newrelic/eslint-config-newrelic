1c9272cbc37673bef9dc6d9ad251945c97eb190e
Bob Evans <robert.evans25@gmail.com>
2022-09-14 10:53:27 -0400

    updated overrides to include node/no-missing-import for versioned tests as we install deps at runtime

--- index.js
+++ index.js
@@ -109,7 +109,8 @@ module.exports = {
         'node/no-missing-require': 'off',
         'node/no-unpublished-require': 'off',
         'node/no-unpublished-import': 'off',
-        'node/no-extraneous-import': 'off'
+        'node/no-extraneous-import': 'off',
+        'node/no-missing-import': 'off',
       }
     },
     {
